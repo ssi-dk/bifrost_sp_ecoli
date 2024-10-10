b0206d2d14cd98b2d90d6f89338e3b6b1fdca404
RAHenriksen <rah@hotmail.dk>
2024-10-10 13:12:03 +0200

    simplistic check of db connection

--- cmd_test.py
+++ cmd_test.py
@@ -11,27 +11,15 @@ import shutil
 
 def test_mongo_connection():
     # Get the MongoDB connection string from the environment variable
-    mongo_connection_string = os.environ["BIFROST_DB_KEY"]
+    
+    client = pymongo.MongoClient(os.environ["BIFROST_DB_KEY"])
 
-    if not mongo_connection_string:
-        print("No MongoDB connection string found in environment variables.")
-        return False
+    print("Attempt to connect to the database")
+    ping = client.admin.command('ping')
+    print("Successfully connected to the MongoDB database.")
 
-    try:
-        # Create a MongoDB client
-        client = pymongo.MongoClient(mongo_connection_string)
-
-        print("Attempt to connect to the database")
-        ping = client.admin.command('ping')
-        print("Successfully connected to the MongoDB database.")
-
-        # Clean up: close the client
-        client.close()
-        return True
-
-    except pymongo.errors.ServerSelectionTimeoutError as e:
-        print("Failed to connect to the MongoDB database:", e)
-        return False
+    # Clean up: close the client
+    client.close()
 
 if __name__ == "__main__":
     # Run the test
