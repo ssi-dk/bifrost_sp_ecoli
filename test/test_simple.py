82d029aaf8980cffbc7df7a17bff204f8681f8d7
RAHenriksen <rah@hotmail.dk>
2024-11-14 10:09:28 +0100

    simple test

--- test/test_simple.py
+++ test/test_simple.py
@@ -5,27 +5,26 @@ from bifrost_sp_ecoli import launcher
 import pymongo
 import os
 import shutil
+import warnings
 
 @pytest.fixture
-def test_cwd_in_class():
-    """Test that checks the bifrost install directory."""
-    print(f"bifrost cwd: {self.bifrost_install_dir}")
-    assert self.bifrost_install_dir != ""
-    
-def test_connection():
-    """Test that checks the bifrost install directory."""
-    assert datahandling.has_a_database_connection()
+def test_cwd():
+    bifrost_install_dir = os.environ["BIFROST_INSTALL_DIR"]
+    print(f"bifrost cwd: {bifrost_install_dir}")
+    assert bifrost_install_dir != ""
+ 
+#def test_connection():
+#    assert datahandling.has_a_database_connection()
 
 # Test class to check the Bifrost setup
 class TestBifrostSetup:
     component_name = "bifrost_sp_ecoli_v.0.0.1"
     # component_name = component_name + "__171019"
 
-    @pytest.fixture(autouse=True)
     def setup(self):
         """Fixture to set up necessary environment variables for testing."""
         self.bifrost_install_dir = os.environ["BIFROST_INSTALL_DIR"]
-        self.test_dir = f"{self.bifrost_install_dir}/bifrost/test_data/output/test__whats_my_species/"
+        self.test_dir = f"{self.bifrost_install_dir}/bifrost/test_data/output/test__bifrost_sp_ecoli/"
         self.r1 = f"{self.bifrost_install_dir}/bifrost/test_data/samples/S1_R1.fastq.gz"
         self.r2 = f"{self.bifrost_install_dir}/bifrost/test_data/samples/S1_R2.fastq.gz"
         self.json_entries = [
