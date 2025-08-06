d9363ff64da218405395d8f668a8a3af5784d72a
Kristoffer Kiil <krki@ssi.dk>
2025-08-06 13:43:43 +0200

    Fix: version string in test_simple

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -22,7 +22,7 @@ def test_cwd():
 
 
 class TestBifrostMinReadCheck:
-    component_name = "bifrost_sp_ecoli_v0.0.1"
+    component_name = "sp_ecoli_v0.0.1"
     # component_name = component_name + "__171019"
 
     bifrost_install_dir = os.environ["BIFROST_INSTALL_DIR"]
